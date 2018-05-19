abe2a4f15ad2d9c8cb48f333e2c58eabfd444af3
John Flinchbaugh <john@hjsoft.com>
2018-05-18 21:16:23 -0400

    find wonderland number

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,28 @@
 (ns wonderland-number.finder)
 
+(defn all-same? [x y]
+  (let
+    [
+      s1 (set (str x))
+      s2 (set (str y))
+    ]
+    (= s1 s2)
+  )
+)
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (first
+    (filter
+      (fn [x]
+        (and
+          (all-same? x (* 2 x))
+          (all-same? x (* 2 x))
+          (all-same? x (* 4 x))
+          (all-same? x (* 5 x))
+          (all-same? x (* 6 x))
+        )
+      )
+      (range 100000 1000000)
+    )
+  )
+)
