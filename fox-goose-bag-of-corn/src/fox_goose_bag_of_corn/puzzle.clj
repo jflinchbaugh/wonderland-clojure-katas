bb9f35548349b39c39e57b9d8dfee8a6bc416637
John Flinchbaugh <john@hjsoft.com>
2018-05-21 22:47:32 -0400

    cheating single solution to fox-goose-corn

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -3,4 +3,22 @@
 (def start-pos [[[:fox :goose :corn :you] [:boat] []]])
 
 (defn river-crossing-plan []
-  start-pos)
+  [
+    [[:fox :goose :corn :you] [:boat] []]
+    [[:corn :fox] [:boat :you :goose] []]
+    [[:corn :fox] [:boat] [:you :goose]]
+    [[:fox :corn] [:boat :you] [:goose]]
+    [[:fox :corn :you] [:boat ] [:goose]]
+    [[:corn] [:boat :you :fox] [:goose]]
+    [[:corn] [:boat] [:goose :you :fox]]
+    [[:corn] [:boat :goose :you] [:fox]]
+    [[:corn :you :goose] [:boat] [:fox]]
+    [[:goose] [:you :corn :boat] [:fox]]
+    [[:goose] [:boat] [:you :corn :fox]]
+    [[:goose] [:boat :you] [:corn :fox]]
+    [[:goose :you] [:boat] [:corn :fox]]
+    [[] [:boat :goose :you] [:corn :fox]]
+    [[] [:boat] [:goose :you :corn :fox]]
+    ;[[] [:boat] [:fox :goose :corn :you]]
+  ]
+)
