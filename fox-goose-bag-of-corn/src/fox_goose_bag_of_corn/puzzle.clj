757312caa1d95641aaef6c03d9009f4f0c6e8cd2
John Flinchbaugh <john@hjsoft.com>
2019-01-10 13:09:20 -0500

    capture work in progress

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -21,3 +21,39 @@
     [[] [:boat] [:goose :you :corn :fox]]
   ]
 )
+
+(defn move-east [[west river east] & [carrying]]
+  (let
+    [
+      west-set (set west)
+      river-set (set river)
+      east-set (set east)
+    ]
+    (vec
+      (map (comp vec #(filter (complement nil?) %))
+        (cond
+          (contains? west-set :you carrying) [
+            (vec (disj west-set :you carrying))
+            (conj river :you carrying)
+            east
+          ]
+          (contains? river-set :you carrying) [
+            west
+            (vec (disj river-set :you carrying))
+            (conj east :you carrying)
+          ]
+          (contains? east-set :you :carrying) [west river east]
+          :else [west river east]
+        )
+      )
+    )
+  )
+)
+
+(move-east
+  (->
+    start-pos
+    last
+  )
+  :chicken
+)
